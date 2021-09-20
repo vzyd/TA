16f3fc9a19b0973f02dc68165c6f302e22beb8be
Viktor Zhydchenko <Viktor_Zhydchenko@epam.com>
2021-09-20 07:20:34 +0300

    Resr_Assured - checNotAuthorizedUser added

--- Rest_Assured/src/test/java/TestDemoQA.java
+++ Rest_Assured/src/test/java/TestDemoQA.java
@@ -1,3 +1,4 @@
+
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.specification.RequestSpecification;
@@ -8,16 +9,24 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class TestDemoQA {
 
-    public static Map<String, Object> jsonAsMap = new HashMap<>();
+    public static Map<String, Object> jsonAsMapUser1 = new HashMap<>();
+    public static Map<String, Object> jsonAsMapUser2 = new HashMap<>();
 
     @BeforeTest
     public void prepareLogin(){
-        jsonAsMap.put("userName", "vzyd");
-        jsonAsMap.put("password", "!Qwerty1");
+        UserDTO user1 = new UserDTO("vzyd", "!Qwerty1");
+        jsonAsMapUser1.put("userName", user1.getUserName());
+        jsonAsMapUser1.put("password", user1.getPassward());
+
+        UserDTO user2 = new UserDTO("user2", "qwerty");
+        jsonAsMapUser2.put("userName", user2.getUserName());
+        jsonAsMapUser2.put("password", user2.getPassward());
     }
 
     @Test
@@ -28,14 +37,25 @@ public class TestDemoQA {
                 .assertThat().header("content-type", "application/json; charset=utf-8");
     }
     @Test
-    public void checkAuthorized(){
+    public void checkAuthorizedUser1(){
         given().spec(getRequestSpec())
                 .header("content-type", "application/json")
-                .body(jsonAsMap)
+                .body(jsonAsMapUser1)
                 .when()
                 .post("/Account/v1/Authorized")
                 .then().assertThat().statusCode(200)
                 .body(Matchers.is("true"));
+    }
+
+    @Test
+    public void checNotAuthorizedUser(){
+        given().spec(getRequestSpec())
+                .header("content-type", "application/json")
+                .body(jsonAsMapUser2)
+                .when()
+                .post("/Account/v1/Authorized")
+                .then().assertThat().statusCode(404)
+                .body("message", equalTo("User not found!"));
     }
 
     public static RequestSpecification getRequestSpec(){
